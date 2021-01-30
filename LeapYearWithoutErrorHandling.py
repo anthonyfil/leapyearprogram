c65c67282477475273ec9f6ebb2f576fd1b65584
anthonyfil <56458147+anthonyfil@users.noreply.github.com>
2021-01-30 15:37:58 -0800

    Added both py code

---
+++ LeapYearWithoutErrorHandling.py
@@ -0,0 +1,17 @@
+print("This is a Leap Year Calculator")
+inp = "y"
+#while(not inp.isdigit()):
+print("Enter a Year")
+inp = input()
+year = int(inp)
+if(year%4 == 0):
+    if(year%100 == 0):
+        if(year%400 == 0):
+            print(inp + " Is A Leap Year")  
+        else:
+            print(inp + " Is Not A Leap Year")
+
+    else:
+          print(inp + " Is A Leap Year")
+else:
+    print(inp + " Is Not A Leap Year")
